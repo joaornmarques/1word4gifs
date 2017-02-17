b7f7e0ff2ae08da112844b72b6b8f4f2df594da4
joaornmarques <joaornmarques@gmail.com>
2017-02-17 20:39:18 +0000

    display gifs
    Responsive!!

--- js/gif_request.js
+++ js/gif_request.js
@@ -6,9 +6,10 @@ var request = new XMLHttpRequest;
 var data;
 
 function getGifs(){
-  var gifsHtml = '';
+    var gifsHtml = '';
+    var gifsLink = '';
 	
-	var query = domain + 'sonic' + "&limit=" + 4 + apiKey;
+	var query = domain + 'nyan+cat' + "&limit=" + 4 + apiKey;
 	console.log(query);
 
 	
@@ -20,12 +21,13 @@ function getGifs(){
 			console.log(data);
 			for(var i = 0; i < data.data.length; i++){
                 
-				gifsHtml += '<img class="gif" src = ' + data.data[i].images.original.url +' "title="gif by Giphy">';
+                gifsLink = data.data[i].images.original.url 
+                gifsHtml += '<div class="gif__image" style="background-image: url('+ gifsLink +');"></div>';
 			}
 			//console.log(gifsHtml);
-			document.getElementById("GifContainer").innerHTML = gifsHtml;
+			document.getElementById("gif__container").innerHTML = gifsHtml;
 		} else {
-			alert('Conexão ao Giphy estabelecida, mas não foi possível enviar o pedido');
+			alert('Oh my GIF! Something bad happened...');
 		}
 	};
 
